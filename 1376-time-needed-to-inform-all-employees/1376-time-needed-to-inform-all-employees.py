9fd4f4f2fb03325334003f4856a229d68f24dc43
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-10-18 22:03:44 +0530

    Time: 4595 ms (5.04%), Space: 53.9 MB (19.16%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,55 @@
+class Solution:
+    def __init__(self):
+        self.time=0
+        self.q=[]
+        self.visited=set()
+        self.levels={}
+        self.max_value=0
+    def getneighbor(self,u,manager,informTime):
+        neighbor=[]
+        for i in range(len(manager)):
+            if manager[i]==u:
+                neighbor.append(i)
+        #print("neighbor=",neighbor)
+        return neighbor
+        
+    def bfs(self,headID,path,informTime,manager,d):
+        self.levels[headID]=0
+        self.q.append(headID)
+        self.visited.add(headID)
+        max_value=0
+        while(self.q):
+            u=self.q.pop(0)
+            #print("u=",u)
+            
+            #neighbor=self.getneighbor(u,manager,informTime)
+            if u in d:
+                for i in d[u]:
+                #print("i=",i)
+                #for i in neighbor:
+                    if i not in self.visited:
+                        #print("i=",i)
+                        self.q.append(i)
+                        self.levels[i]=self.levels[u]+informTime[u]
+                        #print("self.levels=",self.levels)
+                        self.visited.add(i)
+                        max_value=max(max_value,self.levels[i])    
+        return max_value
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        path=0
+        d={}
+        for i in range(len(manager)):
+            if manager[i] in d:
+                if manager[i]!=-1:
+                    d[manager[i]].append(i)
+            else:
+                if manager[i]!=-1:
+                    d[manager[i]]=[i]
+        if d=={}:
+            return 0
+        #print("d=",d)
+        count=self.bfs(headID,path,informTime,manager,d)
+        
+        #print(self.levels)
+        #return max(self.levels.values())
+        return count
