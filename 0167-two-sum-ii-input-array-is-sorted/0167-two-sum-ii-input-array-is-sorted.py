1104e1ae9f1d4fac3e6ada93ba5a413377cbf571
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-10-19 16:32:12 +0530

    Time: 307 ms (30.59%), Space: 15.1 MB (9.76%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,13 +3,17 @@ class Solution:
         ptr1=0
         ptr2=len(numbers)-1
         l=[]
-        
+    
         while(True):
             if numbers[ptr1]+numbers[ptr2]==target:
-                return [ptr1+1,ptr2+1]
-            if numbers[ptr1]+numbers[ptr2]<target:
+                l.append(ptr1+1)
+                l.append(ptr2+1)
+                break
+            elif(numbers[ptr1]+numbers[ptr2]<target):
                 ptr1+=1
             else:
                 ptr2-=1
             if ptr1==ptr2:
                 break
+        return l
+            
