46232d1bb51e4d83ee453e3590793f1ce43b619f
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-10-26 11:23:54 +0530

    Time: 77 ms (32.52%), Space: 14 MB (32.70%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,8 +5,7 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr1=list1
-        curr2=list2
+        
         head3=ListNode()
         
         
@@ -16,35 +15,32 @@ class Solution:
             return list1
         if not list2 and not list1:
             return None
-        if curr1.val<=curr2.val:
-            head3.val=curr1.val
+        
+        if list1.val<=list2.val:
+            head3.val=list1.val
             curr3=head3
-            curr1=curr1.next
+            list1=list1.next
         else:
-            head3.val=curr2.val
+            head3.val=list2.val
             curr3=head3
-            curr2=curr2.next
+            list2=list2.next
        
         
-        while(curr1 and curr2):
-            if curr1.val<curr2.val:
-                p=ListNode()
-                p.val=curr1.val
-                curr3.next=p
-                curr3=p
-                curr1=curr1.next
+        while(list1 and list2):
+            if list1.val<list2.val:
+                curr3.next=list1
+                curr3=list1
+                list1=list1.next
                
             else:
-                p=ListNode()
-                p.val=curr2.val
-                curr3.next=p
-                curr3=p
-                curr2=curr2.next
-                
-        if(curr1 and not curr2):
-            curr3.next=curr1
-        if curr2 and not curr1:
-            curr3.next=curr2
+                curr3.next=list2
+                curr3=list2
+                list2=list2.next
+           
+        if(list1 and not list2):
+            curr3.next=list1
+        if list2 and not list1:
+            curr3.next=list2
         
         
        
