4bbd8fdd79b15f9c5a43be9bd8bd435cc67d072b
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-10-27 13:45:23 +0530

    Time: 6092 ms (8.74%), Space: 21.6 MB (86.15%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,10 +1,44 @@
-from heapq import *
+
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        l=[]
-        heapify(nums)
-        for i in range(len(nums)):
-            
-            a=heappop(nums)
-            l.append(a)
-        return l
+    def merge(self,arr,low,mid,high):
+        i=low
+        j=mid+1
+        aux=[]
+    
+        while(i<=mid and j<=high):
+            if arr[i]<=arr[j]:
+                aux.append(arr[i])
+                i+=1
+            else:
+                aux.append(arr[j])
+                j+=1
+        while(i<=mid):
+            aux.append(arr[i])
+            i+=1
+        while(j<=high):
+            aux.append(arr[j])
+            j+=1
+        count=0
+        for i in range(low,high+1):
+            arr[i]=aux[count]
+            count+=1
+            if count==len(aux):
+                break
+        #print("aux=",aux)
+        #print("arr=",arr)   
+        
+    
+    def mergesort(self,arr,low,high):
+    
+        if low>=high:
+            return
+        mid=(low+high)//2
+        self.mergesort(arr,low,mid)
+        self.mergesort(arr,mid+1,high)
+        self.merge(arr,low,mid,high)
+
+    def sortArray(self, arr: List[int]) -> List[int]:
+        low=0
+        high=len(arr)-1
+        self.mergesort(arr,low,high)
+        return arr
