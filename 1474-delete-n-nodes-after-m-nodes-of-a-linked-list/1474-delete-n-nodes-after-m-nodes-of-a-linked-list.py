16873ae6733db924a5e226852df3d3fc32d654f5
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-11-05 18:38:15 +0530

    Time: 186 ms (5.11%), Space: 16.9 MB (23.02%) - LeetHub

---
+++ 1474-delete-n-nodes-after-m-nodes-of-a-linked-list/1474-delete-n-nodes-after-m-nodes-of-a-linked-list.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteNodes(self, head: ListNode, m: int, n: int) -> ListNode:
+        
+            sentinel = ListNode()
+            tail = sentinel
+            prev=None
+            curr = head
+
+            while (head):
+                count1 = 0
+                count2 = 0
+                while(curr and count1<m):
+                    prev=curr
+                    curr=curr.next
+                    count1+=1
+                prev.next=None
+                tail.next=head
+                tail=prev
+                #self.display(head)
+                #head=curr
+
+                while(curr and count2<n):
+                    prev=curr
+                    curr=curr.next
+                    count2+=1
+                head=curr
+            head=sentinel.next
+            return head
+
+            
+            
+            
