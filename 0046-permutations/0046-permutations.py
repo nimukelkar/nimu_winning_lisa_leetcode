cdbdbac195f36992049c222bba7b18fd6e077194
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-11-17 14:42:43 +0530

    Time: 44 ms (90.23%), Space: 14 MB (84.97%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        l=[]
+        
+        def dfs(menu,i,slate):
+            if i==n:
+                l.append(slate.copy())
+                return
+            
+            for j in range(i,n):
+                menu[j],menu[i]=menu[i],menu[j]
+                
+                slate.append(menu[i])
+                dfs(menu,i+1,slate)
+                slate.pop()
+                menu[i],menu[j]=menu[j],menu[i]
+        
+        
+        dfs(nums,0,[])
+        return l
+                
