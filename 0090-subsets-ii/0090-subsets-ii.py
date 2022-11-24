ba220efca5d020bc5bffa7d545a8fbd63ba74efa
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-11-24 12:09:21 +0530

    Time: 39 ms (93.37%), Space: 14.1 MB (93.30%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -6,16 +6,20 @@ class Solution:
         
         def dfs(menu,i,slate):
             if i==n:
-                if slate not in l:
-                    l.append(slate.copy())
+                l.append(slate.copy())
                 return
             
             
-            dfs(menu,i+1,slate)
-            
+            #Take menu[i] and append to slate
             slate.append(menu[i])
             dfs(menu,i+1,slate)
             slate.pop()
+            
+            while(i<n-1 and menu[i]==menu[i+1]):
+                i+=1
+            #Take menu[i] and discard it
+            dfs(menu,i+1,slate)
+                    
         dfs(nums,0,[])
         return l
                 
