50f52548fb1ec3a45829e4fe2536dc0b8e44b440
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-11-25 12:45:11 +0530

    Time: 5779 ms (5.09%), Space: 243.5 MB (5.09%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n=len(nums[0])
+        #print(n)
+        l=[]
+        def dfs(menu,i,slate):
+            
+            if i==n:
+                l.append("".join(slate.copy()))
+                return
+            
+            
+            for j in range(len(menu)):
+                slate.append(menu[j])
+                dfs(menu,i+1,slate)
+                slate.pop()
+            return
+        menu=["0","1"]
+        dfs(menu,0,[])
+        for i in l:
+            if i not in nums:
+                return i
+                    
+                
