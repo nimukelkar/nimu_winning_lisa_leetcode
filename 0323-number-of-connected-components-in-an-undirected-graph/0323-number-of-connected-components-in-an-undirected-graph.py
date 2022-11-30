09b0812c10993bef277ececed3f1da974d06a6f3
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-11-30 14:18:05 +0530

    Time: 102 ms (95.62%), Space: 15.5 MB (56.44%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,36 +1,39 @@
-from collections import *
+from collections import defaultdict
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         d=defaultdict(list)
-       
-        
-        visited=set()
-        
+        visited={}
         for u,v in edges:
             d[u].append(v)
             d[v].append(u)
-        #print(d)
+        
+        
         def bfs(node):
             q=deque()
             q.append(node)
-        
+            
             while(q):
                 u=q.popleft()
                 for neighbor in d[u]:
                     if neighbor not in visited:
-                        visited.add(neighbor)
+                        visited[neighbor]=visited[u]+1
                         q.append(neighbor)
-            return
-                    
         
         count=0
         for i in range(n):
             if i not in visited:
+                visited[i]=0
                 bfs(i)
                 count+=1
         return count
-            
-                
-                     
-                  
+                        
+                        
+        
+        
+        
+        
+        
+        
+        
+        
         
