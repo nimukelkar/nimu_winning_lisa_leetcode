f7798fcfc358d17ea2a23ab8c2451bab7bab18ba
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-12-02 10:24:39 +0530

    Time: 1123 ms (12.09%), Space: 23.7 MB (13.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,52 +1,51 @@
 from collections import *
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
+        #Connected components
         visited={}
         
         def getneighbor(node):
+            nx,ny=node
             l=[]
-            ux,uy=node
-            if ux+1<m and grid[ux+1][uy]=="1":
-                l.append((ux+1,uy))
-            if uy+1<n and grid[ux][uy+1]=="1":
-                l.append((ux,uy+1))
-            if ux-1>=0 and grid[ux-1][uy]=="1":
-                l.append((ux-1,uy))
-            if uy-1>=0  and grid[ux][uy-1]=="1":
-                l.append((ux,uy-1))
-            
-            
+            if nx+1<=m-1 and grid[nx+1][ny]=="1":
+                l.append((nx+1,ny))
+            if ny+1<=n-1 and grid[nx][ny+1]=="1":
+                l.append((nx,ny+1))
+            if nx-1>=0 and grid[nx-1][ny]=="1":
+                l.append((nx-1,ny))
+            if ny-1>=0 and grid[nx][ny-1]=="1":
+                l.append((nx,ny-1))
+                
             return l
+        
+        
+        def dfs(node):
+            ux,uy=node
+            # Check neighbors of ux,uy
             
+            neighborlist=getneighbor(node)
             
-        def bfs(start):
-            i,j=start
-            q=deque()
-            q.append((i,j))
-            while(q):
-                ux,uy=q.popleft()
-                neighborslist=getneighbor((ux,uy))
-                #print(neighborslist)
-                for neighbor in neighborslist:
-                    nx,ny=neighbor
-                    if neighbor not in visited:
-                        visited[neighbor]=visited[(ux,uy)]+1
-                        q.append((nx,ny))
-        
-                                
+            for neighbor in neighborlist:
+                if neighbor not in visited:
+                    visited[neighbor]=visited[(ux,uy)]+1
+                    dfs(neighbor)
         
         
+        
+        
+        
+        
+        
+        
+        
+        m=len(grid)
+        n=len(grid[0])
         count=0
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited and grid[i][j]=="1":
+                if ((i,j) not in visited and grid[i][j]=="1"):
                     visited[(i,j)]=0
-                    bfs((i,j))
+                    dfs((i,j))
                     #print("visited=",visited)
                     count+=1
-        
         return count
-                    
-                
