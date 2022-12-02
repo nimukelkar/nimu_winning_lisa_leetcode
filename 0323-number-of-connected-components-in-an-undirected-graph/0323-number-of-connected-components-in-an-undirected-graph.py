f8821cef7a2880c3845e1e389949ea2435c5519e
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-12-02 10:05:01 +0530

    Time: 216 ms (59.25%), Space: 17.2 MB (18.89%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -7,30 +7,32 @@ class Solution:
             d[u].append(v)
             d[v].append(u)
         
-        
-        def bfs(node):
-            q=deque()
-            q.append(node)
+        #print("d=",d)
+        def dfs(i):
             
-            while(q):
-                u=q.popleft()
-                for neighbor in d[u]:
-                    if neighbor not in visited:
-                        visited[neighbor]=visited[u]+1
-                        q.append(neighbor)
+            for neighbor in d[i]:
+                if neighbor not in visited:
+                    visited[neighbor]=visited[i]+1
+                    dfs(neighbor)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         count=0
         for i in range(n):
             if i not in visited:
                 visited[i]=0
-                bfs(i)
+                dfs(i)
                 count+=1
         return count
-                        
-                        
-        
-        
-        
+            
         
         
         
