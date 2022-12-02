a2ca5f1c137e86d86c97d62133506b21e3c8a083
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2022-12-02 22:42:16 +0530

    Time: 2092 ms (22.36%), Space: 15.3 MB (40.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,90 +1,88 @@
+from collections import *
 class Solution:
-    def __init__(self):
-        self.visited=set()
-        self.q=[]
-        self.level={}
-    
-    def getneighbors(self,u,grid):
-        m=len(grid)
-        n=len(grid[0])
-        neighbors=[]
-        ux,uy=u[0],u[1]
-        
-        
-        nx=ux
-        ny=uy+1
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        nx=ux
-        ny=uy-1
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        
-        nx=ux+1
-        ny=uy
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        
-        nx=ux-1
-        ny=uy
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        
-        nx=ux-1
-        ny=uy-1
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        
-        nx=ux+1
-        ny=uy+1
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        
-        nx=ux-1
-        ny=uy+1
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        
-        
-        nx=ux+1
-        ny=uy-1
-        if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
-            neighbors.append((nx,ny))
-        #print("neighbors=",neighbors)
-        return neighbors
-    
-    
-    def bfs(self,start,end,grid):
-        self.level[start]=1
-        while self.q:
-            u=self.q.pop(0)
-            if u==end:
-                return self.level[u]
-            else:
-                neighbors=self.getneighbors(u,grid)
-                for i in neighbors:
-                    if i not in self.visited:
-                        self.q.append(i)
-                        self.visited.add(i)
-                        self.level[i]=self.level[u]+1
-        return -1
-            
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         
-        m=len(grid)
-        n=len(grid[0])
+        
+        def getneighbor(u):
+            neighbors=[]
+            ux,uy=u
+            nx=ux
+            ny=uy+1
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux+1
+            ny=uy
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux+1
+            ny=uy+1
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux-1
+            ny=uy-1
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux+1
+            ny=uy-1
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux-1
+            ny=uy+1
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux
+            ny=uy-1
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            
+            nx=ux-1
+            ny=uy
+            if nx>=0 and ny>=0 and nx<=m-1 and ny<=n-1 and grid[nx][ny]!=1:
+                neighbors.append((nx,ny))
+            #print("neighbors=",neighbors)
+            return neighbors
+                
+        
+        
+        
+        def bfs(start):
+            nx,ny=start
+            q=deque()
+            q.append(start)
+            
+            while(q):
+                u=q.popleft()
+                #print("u=",u)
+                neighborlist=getneighbor(u)
+               
+                for neighbor in neighborlist:
+                    if neighbor==target:
+                        visited[neighbor]=visited[u]+1
+                        return visited[neighbor]
+                    if neighbor not in visited:
+                        visited[neighbor]=visited[u]+1
+                        q.append(neighbor)
+            return -1
+        
+        
+        
+        
+       
+        n,m=len(grid),len(grid[0])
+        target=(m-1,n-1)
+        visited={}
         start=(0,0)
         if grid[0][0]==1:
             return -1
-        end=(m-1,n-1)
-        self.visited.add(start)
-        self.q.append(start)
-
-        count=self.bfs(start,end,grid)
-        return count
+        if grid[0][0]==0 and n==1:
+            return 1
+        visited[start]=1
+        ans=bfs(start)
+        return ans
+        
