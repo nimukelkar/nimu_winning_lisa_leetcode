76ac4455c9f5ce97c0241d24f330da3715c75d6a
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-01-27 12:34:06 +0530

    Time: 43 ms (25.34%), Space: 13.9 MB (57.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        table=[0]*n
-        table[0]=nums[0]
-        if n==2:
+        table=[0 for i in range(len(nums)+1)]
+        l=len(nums)
+        if l==1:
+            table[0]=nums[0]
+            return table[0]
+        elif l==2:
+            
             table[1]=max(nums[0],nums[1])
             return table[1]
-        for i in range(1,n):
-            table[i]=max(nums[i]+table[i-2],table[i-1])
-        print(table)
-        return table[n-1]
+        
+        else:
+            table[0]=nums[0]
+            table[1]=max(nums[0],nums[1])
             
+            for i in range(2,len(nums)):
+                table[i]=max(nums[i]+table[i-2],table[i-1])
+            
+            return table[l-1]
+                
+            
+        
