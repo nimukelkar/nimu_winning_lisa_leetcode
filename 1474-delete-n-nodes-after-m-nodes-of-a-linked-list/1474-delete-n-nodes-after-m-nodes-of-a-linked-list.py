c619e19fc629868b4c7c5fb2da483705e58dfe63
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-02-04 13:32:36 +0530

    Time: 75 ms (48.20%), Space: 16.8 MB (91.89%) - LeetHub

--- 1474-delete-n-nodes-after-m-nodes-of-a-linked-list/1474-delete-n-nodes-after-m-nodes-of-a-linked-list.py
+++ 1474-delete-n-nodes-after-m-nodes-of-a-linked-list/1474-delete-n-nodes-after-m-nodes-of-a-linked-list.py
@@ -5,33 +5,28 @@
 #         self.next = next
 class Solution:
     def deleteNodes(self, head: ListNode, m: int, n: int) -> ListNode:
+        sentinel=ListNode()
+        tail=sentinel
         
-            sentinel = ListNode()
-            tail = sentinel
-            prev=None
-            curr = head
-
-            while (head):
-                count1 = 0
-                count2 = 0
-                while(curr and count1<m):
-                    prev=curr
-                    curr=curr.next
-                    count1+=1
-                prev.next=None
-                tail.next=head
-                tail=prev
-                #self.display(head)
-                #head=curr
-
-                while(curr and count2<n):
-                    prev=curr
-                    curr=curr.next
-                    count2+=1
-                head=curr
-            head=sentinel.next
-            return head
-
+        prev=None
+        curr=head
+        
+        while(head):
+            
+            count1,count2=0,0
+            while(curr and count1<m):
+                prev=curr
+                curr=curr.next
+                count1+=1
+            prev.next=None
+            tail.next=head
+            tail=prev
             
             
-            
+            while(curr and count2<n):
+                prev=curr
+                curr=curr.next
+                count2+=1
+            head=curr
+        head=sentinel.next
+        return head
