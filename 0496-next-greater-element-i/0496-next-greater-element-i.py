fc6e5346e22684217eff220918a7901a19ca2310
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-02-23 15:42:00 +0530

    Time: 55 ms (58.58%), Space: 14.1 MB (48.24%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,23 +1,27 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n=len(nums2)
-        d={}
-        stack=[]
-        l=[]
-        for i in range(n):
         
-            while (stack and nums2[i]>stack[-1]):
-                #Make a match.
-                d[stack[-1]]=nums2[i]
-                stack.pop()
-
-            stack.append(nums2[i])
-
-        #print("stack=",stack)
-        while(stack):
-            d[stack[-1]]=-1
-            stack.pop()
-        #print(d)
+        stk=[]
+        d={}
+        ans=[]
+        
+        for i in range(len(nums2)):
+            
+            while(stk and nums2[i]>stk[-1]):
+                d[stk[-1]]=nums2[i]
+                stk.pop()
+            
+            stk.append(nums2[i])
+        
+        
+        while stk:
+            d[stk[-1]]=-1
+            stk.pop()
+        
+        
         for i in nums1:
-            l.append(d[i])
-        return l
+            if i in d:
+                ans.append(d[i])
+        
+        return ans
+                
