acfb9be49d8e2da296598d8cd8ac4d3681dc2e91
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-02-27 16:15:27 +0530

    Time: 1334 ms (19.53%), Space: 51.4 MB (5.41%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -0,0 +1,65 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        
+        stk=[]
+        n=len(heights)
+        final=[0 for i in range(n)]
+        rspan=[0 for i in range(n)]
+        ans=[0 for i in range(n)]
+        idx_left=[0 for i in range(n)]
+        right_result=[1 for i in range(len(heights))]
+        left_result=[1 for i in range(len(heights))]
+        
+        #Find next smaller element
+        for i in range(n):
+            
+            
+            while(stk and heights[i]<heights[stk[-1]]):
+                idx=stk.pop()
+                ans[idx]=i-idx
+            stk.append(i)
+            
+        
+        #print(ans)
+        for i in range(n):
+            if ans[i]==0:
+                rspan[i]=n-i
+            else:
+                rspan[i]=ans[i]
+      
+        for i in range(n):
+            right_result[i]=rspan[i]*heights[i]
+        #print("Right span=",rspan) 
+        #Find prev smaller element
+        
+        heights=heights[::-1]
+        ans2=[0 for i in range(n)]
+        left_span=[0 for i in range(n)]
+        stk2=[]
+        for i in range(n):
+            while(stk2 and heights[i]<heights[stk2[-1]]):
+                idx=stk2.pop()
+                ans2[idx]=i-idx
+
+            stk2.append(i)
+        ans2=ans2[::-1]
+        heights=heights[::-1]
+        
+        #print("ans2=",ans2)
+        for i in range(n):
+            if ans2[i]==0:
+                left_span[i]=i+1
+            else:
+                left_span[i]=ans2[i]
+        #print("Left_span=",left_span)
+        for i in range(n):
+            left_result[i]=heights[i]*left_span[i]
+        #print("left_result=",left_result)
+        for i in range(n):
+            final[i]=left_result[i]+right_result[i]-heights[i]
+        #print("Right result=",right_result)
+        #print("final=",final)
+        return max(final)
+            
+
+                
