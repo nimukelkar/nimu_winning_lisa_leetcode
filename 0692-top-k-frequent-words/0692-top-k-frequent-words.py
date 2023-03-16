9f625870fe6cfea365db18843ba3ce069067feb5
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-03-16 15:49:25 +0530

    Time: 57 ms (70.52%), Space: 14.1 MB (17.71%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,20 +1,18 @@
+from heapq import *
+from collections import *
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        d={}
+        d=defaultdict(int)
         l=[]
-        words.sort()
-        count=0
-        
         for i in words:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        d=dict(sorted(d.items(),key=lambda x:x[1],reverse=True))
-        print(d)
-        for i in d:
-            count+=1
-            l.append(i)
-            if count==k:
-                return l
-                      
+            d[i]+=1
+        
+        #print(d)
+        #Use a priority queue
+        d=sorted(d.items(),key=lambda x:(-x[1],x[0]))
+        
+        for i in range(k):
+            l.append(d[i][0])
+        return l
+        
+            
