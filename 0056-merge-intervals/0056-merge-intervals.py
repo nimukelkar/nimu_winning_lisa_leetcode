aef7016a345b6a09c0c587815652da69344ccf9a
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-03-19 13:28:45 +0530

    Time: 150 ms (66.59%), Space: 18.1 MB (77.05%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,27 +1,27 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals=sorted(intervals,key=lambda x:x[0])
-        anslist=[]
-        
-        anslist.append(intervals[0])
-        
-        prev_start=anslist[-1][0]
-        prev_end=anslist[-1][1]
-        for i in range(1,len(intervals)):
+        ans=[]
+        ans.append(intervals[0])
+        prev_start=ans[-1][0]
+        prev_end=ans[-1][1]
+        i=1
+        while(i<len(intervals)):
             curr_start=intervals[i][0]
             curr_end=intervals[i][1]
             
-            if curr_start>prev_end:
+            if(curr_start>prev_end):
                 #No overlap
-                anslist.append(intervals[i])
-                prev_start=anslist[-1][0]
-                prev_end=anslist[-1][1]
+                ans.append(intervals[i])
+                prev_start=intervals[i][0]
+                prev_end=intervals[i][1]
+                i+=1
             else:
-                #Merge
-                anslist[-1][1]=max(anslist[-1][1],intervals[i][1])
-                prev_start=anslist[-1][0]
-                prev_end=anslist[-1][1]
-        return anslist
-                                    
-                                    
+                #Overlap
+                ans[-1][1]=max(ans[-1][1],curr_end)
+                prev_start=ans[-1][0]
+                prev_end=ans[-1][1]
+                i+=1
+        return ans
+                                
             
