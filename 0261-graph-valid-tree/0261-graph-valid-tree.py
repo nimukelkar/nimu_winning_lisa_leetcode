60af3b0ca691b9513ebfaa148359453cbf361e37
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-03-19 11:37:15 +0530

    Time: 101 ms (42.74%), Space: 15.6 MB (54.66%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,24 +1,20 @@
-from collections import *
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        #No of connected components=1
-        #No cycles
-        visited=set()
-        flag=[True]
-        parents={}
         d=defaultdict(list)
+        parents={}
+        flag=[True]
+        visited=set()
         for u,v in edges:
             d[u].append(v)
             d[v].append(u)
-        #print(d)
+        print(d)
+        
         
         def bfs(node):
             q=deque()
             q.append(node)
-            
             while(q):
                 u=q.popleft()
-                
                 for neighbor in d[u]:
                     if neighbor not in visited:
                         visited.add(neighbor)
@@ -27,16 +23,19 @@ class Solution:
                     else:
                         if parents[u]!=neighbor:
                             flag[0]=[False]
+                        
+                    
+        
         count=0
-        for i in range(n):
+        for i in range (n):
             if i not in visited:
                 visited.add(i)
                 bfs(i)
                 count+=1
+        print("count=",count)
         if count==1 and flag[0]==True:
             return True
         return False
-                
-                            
-                        
+               
+        
         
