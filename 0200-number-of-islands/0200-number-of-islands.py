2c5bcb85c8d6cfd58d9f4704b6a4ba1812fb3218
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-03-20 16:50:43 +0530

    Time: 300 ms (70.93%), Space: 23.9 MB (13.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,44 +1,54 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        count=0
         visited={}
-    
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
         def getneighbor(node):
             nx,ny=node
             l=[]
-            if nx+1<=m-1 and grid[nx+1][ny]=="1":
+            if nx+1<m and grid[nx+1][ny]=="1":
                 l.append((nx+1,ny))
-            if ny+1<=n-1 and grid[nx][ny+1]=="1":
+            if ny+1<n and grid[nx][ny+1]=="1":
                 l.append((nx,ny+1))
             if nx-1>=0 and grid[nx-1][ny]=="1":
                 l.append((nx-1,ny))
             if ny-1>=0 and grid[nx][ny-1]=="1":
                 l.append((nx,ny-1))
-                
             return l
+            
+            
+        
         
         def dfs(node):
             neighborlist=getneighbor(node)
-            
             for neighbor in neighborlist:
                 if neighbor not in visited:
-                    visited[neighbor]=visited[node]+1
+                    visited[neighbor]=node
                     dfs(neighbor)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        count=0
+            
+        
+        
         m,n=len(grid),len(grid[0])
+        count=0
+        
         for i in range(m):
             for j in range(n):
-                if ((i,j) not in visited and grid[i][j]=="1"):
+                if grid[i][j]=="1" and (i,j) not in visited:
                     visited[(i,j)]=0
                     dfs((i,j))
                     count+=1
         return count
-        
