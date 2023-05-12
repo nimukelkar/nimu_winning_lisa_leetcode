10058e8c04ec44f7199f8b1088de4e37aa727271
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-05-12 20:46:35 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.py
+++ Insertion Sort - GFG/insertion-sort.py
@@ -1,18 +1,23 @@
 #Sort the array using insertion sort
 
 class Solution:
-    def insertionSort(self,arr,n):
+   
         
+    #Function to sort the list using insertion sort algorithm.    
+    def insertionSort(self, alist, n):
+        #code here
         for i in range(1,n):
-            
             j=i-1
             
-            while(j>=0 and arr[j]>arr[j+1]):
-                arr[j],arr[j+1]=arr[j+1],arr[j]
+            while(j>=0 and alist[j]>alist[j+1]):
+                alist[j],alist[j+1]=alist[j+1],alist[j]
                 j-=1
+        
+        return alist
+            
             
         
-    
+
 
 #{ 
  # Driver Code Starts
