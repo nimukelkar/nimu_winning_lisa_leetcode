eeddae2c65961045510d7624964bd56d7640b117
Nirmayi Kelkar <60577980+nimukelkar@users.noreply.github.com>
2023-05-12 22:34:32 +0530

    Added solution - LeetHub

--- Nth catalan number - GFG/nth-catalan-number.py
+++ Nth catalan number - GFG/nth-catalan-number.py
@@ -4,25 +4,20 @@ class Solution:
     #Function to find the nth catalan number.
     def findCatalan(self,n):
         #return the nth Catalan number.
+        #C2=C01+C10
+        #C3=C0C2+C1C1+C2C0
         table=[0 for i in range(n+1)]
-        for j in range(n+1):
-            if j==0:
-                table[0]=1
-                continue
-            
-            
-            elif j==1:
-                table[1]=1
-                continue
+        table[0]=1
+        table[1]=1
+        
+        for i in range(2,n+1):
+            s=0
+            for j in range(i): #0,1
+                s=s+table[j]*table[i-j-1]
+            table[i]=s
+        
+        return table[-1]
                 
-            else:
-                ans=0
-                for i in range(j):
-                    ans+=table[i]*table[j-i-1]
-            table[j]=ans
-        #print(table)
-        return table[n]
-            
 
 
 #{ 
